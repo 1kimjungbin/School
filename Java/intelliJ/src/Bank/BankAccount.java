988e4b614c94c5619f198379cef587a039fce9f2
1kimjungbin <1kimjungbin@gmail.com>
2022-05-23 09:47:08 +0900

    20220523

---
+++ Java/intelliJ/src/Bank/BankAccount.java
@@ -0,0 +1,29 @@
+package Bank;
+
+public class BankAccount {
+    private int balance;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public boolean withdraw(int amount) {
+        if(balance>=amount) {
+            balance -= amount;
+            return true;
+        }
+
+        return false;
+    }
+
+    // 입금
+
+    public boolean deposit(int amount) {
+        balance += amount;
+        return true;
+    }
+}
---
+++ Java/intelliJ/src/Bank/MinimumBalanceAccount.java
@@ -0,0 +1,27 @@
+package Bank;
+
+public class MinimumBalanceAccount extends BankAccount {
+
+
+    private int minimum;
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+
+    @Override
+
+    public boolean withdraw(int amount) {
+        if(getBalance() - amount < minimum) {
+            System.out.println("적어도" + minimum + " 원은 남겨야한다.");
+            return false;
+        }
+
+        setBalance(getBalance() - amount);
+        return true;
+    }
+}
