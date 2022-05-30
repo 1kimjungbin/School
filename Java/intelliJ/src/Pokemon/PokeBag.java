d309e57948c46d2937b65bbeb115a33041fbf7d9
1kimjungbin <1kimjungbin@gmail.com>
2022-05-30 17:34:13 +0900

    PokemonGO

--- Java/intelliJ/src/Pokemon/PokeBag.java
+++ Java/intelliJ/src/Pokemon/PokeBag.java
@@ -1,5 +1,31 @@
-package Pokemon;
+package pokemon;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PokeBag {
 
+    public final HashMap<String, ArrayList<Pokemon>> pokemons = new HashMap<>();
+
+    public ArrayList<Pokemon> getPokemons(String name) {
+        return pokemons.get(name);
+    }
+
+    public void add(Pokemon pokemon) {
+        String name = pokemon.name;
+
+        if(getPokemons(name) == null) {
+            pokemons.put(name, new ArrayList<Pokemon>());
+        }
+
+        getPokemons(name).add(pokemon);
+    }
+
+    public Pokemon getStrongest(String name) {
+        return null;
+    }
+
+    public Pokemon getStrongest() {
+        return null;
+    }
 }
